6e0b84c1e385882fa1903ba8210bad4429c25a54
Jose Hernandez <josemanuelhdez2@gmail.com>
2016-02-01 01:01:32 +0000

    PanelEntityInspector

---
+++ src/gui/simulation/panel/PanelEntityInspector.java
@@ -0,0 +1,36 @@
+package gui.simulation.panel;
+
+import javax.swing.JLabel;
+
+import model.entity.Entity;
+import model.entity.agent.AgentBee;
+
+public class PanelEntityInspector extends SimPanel {
+
+    PanelEntityInspector(Entity ent){
+
+        add(new JLabel("Name:"));
+        add(new JLabel(ent.getName())); 
+       
+        add(new JLabel("PosX:"));
+        add(new JLabel(ent.getPosX().toString()));
+        add(new JLabel("PosY:"));
+        add(new JLabel(ent.getPosY().toString()));
+           
+        switch(ent.getEntityType()){
+        case AGENT_BEE:
+            AgentBee ent2 = (AgentBee)ent;
+            add(new JLabel("Behavior:"));
+            add(new JLabel(ent2.getBehaviour().toString())); 
+            break;
+        case AGENT_WASP:
+            break;
+        case OBJECT_BEEHIVE:
+            break;
+        case OBJECT_FLOWER:
+            break;
+        default:
+            break;
+       }
+    }
+}
